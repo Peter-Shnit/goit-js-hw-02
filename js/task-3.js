93d11df9ace6fcafa3542dbba1e3753a09425ad6
Petro Broker <deltawilmarchumak@gmail.com>
2024-10-01 18:00:55 +0300

    HW-JS-2

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,54 @@
-function getElementWidth(content, padding, border) {
-    const numContent = Number.parseFloat(content);
-    const numPadding = Number.parseFloat(padding);
-    const numBorder = Number.parseFloat(border);
-return (numContent + (numPadding + numBorder) * 2);
+
+                        //  Спосіб №1
+
+// function checkForSpam(message) {
+//     const normMessage = message.toLowerCase();
+//     const hasSpam = normMessage.includes("spam");
+//     const hasSale = normMessage.includes("sale");
+
+// if (hasSpam) {
+//         return true;
+//     }
+//     else if (hasSale) {
+//         return true;
+//     }
+//     else { return false; }
+// }
+
+                        //  Спосіб №2
+
+// function checkForSpam(message) {
+//     const normMessage = message.toLowerCase();
+//     if (normMessage.includes("spam")) {
+//         return true;
+//     }
+//     else if (normMessage.includes("sale")) {
+//         return true;
+//     }
+//     else { return false; }
+// }
+
+                        // Спосіб №3
+
+function checkForSpam(message) {
+    const hasSpam = "spam";
+    const hasSale = "sale";
+    const normMessage = message.toLowerCase();
+
+    if (normMessage.includes(hasSpam)) {
+        return true;
+    }
+    else if (normMessage.includes(hasSale)) {
+        return true;
+    }
+    else { return false; }
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
